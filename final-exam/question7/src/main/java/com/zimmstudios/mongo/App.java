0821c31efe988192d40e5e0ec1d71dc913da5897
Travis Zimmerman <travis.zimmerman30@gmail.com>
2015-07-13 21:28:59 -0400

    Committing solution for question 7.

--- final-exam/question7/src/main/java/com/zimmstudios/mongo/App.java
+++ final-exam/question7/src/main/java/com/zimmstudios/mongo/App.java
@@ -1,5 +1,13 @@
 package com.zimmstudios.mongo;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import static com.mongodb.client.model.Filters.eq;
+import java.util.ArrayList;
+import java.util.List;
+import org.bson.Document;
+
 /**
  * Hello world!
  *
@@ -8,6 +16,44 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        MongoClient client = new MongoClient();
+        MongoDatabase db = client.getDatabase("final7");
+        MongoCollection<Document> albumsCollection = db.getCollection("albums");
+        MongoCollection<Document> imagesCollection = db.getCollection("images");
+        
+        List<Document> allImages = imagesCollection
+                .find()
+                .into(new ArrayList<Document>());
+        List<Document> allAlbums = albumsCollection
+                .find()
+                .into(new ArrayList<Document>());
+        
+        int numDeleted = 0;
+        
+        for (Document image : allImages) {
+            
+            boolean orphan = true;
+            
+            double id = image.getDouble("_id");
+            
+            for (Document album : allAlbums) {
+                
+                List<Double> imageList =
+                    (List<Double>) album.get("images");
+                
+                if (imageList.contains(id)) {
+                    orphan = false;
+                }
+                
+            }
+            
+            if (orphan) {
+                imagesCollection.deleteOne(eq("_id", id));
+                System.out.println("Deleted image with _id: " + id);
+                numDeleted++;
+            }
+        }
+        
+        System.out.println("Num deleted: " + numDeleted);
     }
 }
